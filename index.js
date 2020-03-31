c4c3861e8229921f21510cf560a73631053bca18
Carl Dungca <carl.dungca@yahoo.com>
2020-03-31 15:17:15 -0700

    Updated index.js with JSON middleware and specified database

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ const app = express();
 const port = 3000;
 const db = mongoose.connection;
 
-mongoose.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://127.0.0.1:27017/socialmedia', { useNewUrlParser: true, useUnifiedTopology: true });
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('Connected to mongoDB'));
 
 const posts = require('./routes/posts');
 
+app.use(express.json());
 app.use('/api/posts', posts);
 
 app.get('/', (req, res) => res.send('Welcome to a placeholder social media site!'));
