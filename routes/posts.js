1f541eb5e6075ca3b1ae4f8f0daaca4566e81b7b
Carl Dungca <carl.dungca@yahoo.com>
2020-03-31 15:39:54 -0700

    Added try/catch blocks to posts routes

--- routes/posts.js
+++ routes/posts.js
@@ -11,20 +11,36 @@ router.get('/', async (req, res) => {
 
 // In this get request, we need to check if the ID exists
 router.get('/:postId', async (req, res) => {
-	const post = await posts.findById(req.params.postId);
-	res.send(post);
+	let post
+	try {
+		post = await posts.findById(req.params.postId);
+		res.send(post);
+	}
+	catch {
+		if (!post) res.status(404).send('Could not find the specified user.')
+	}
 });
 
 // In a post request, we need validation
 router.post('/', async (req, res) => {
-	const newPost = new posts(_.pick(req.body, ['title', 'author', 'body']));
-	await newPost.save();
-	res.send(newPost);
+	try {
+		const newPost = new posts(_.pick(req.body, ['title', 'author', 'body']));
+		await newPost.save();
+		res.send(newPost);
+	} catch (ex) {
+		res.status(400).send(ex);
+	}
 });
 
 router.delete('/:postId', async (req, res) => {
-	const post = await posts.findByIdAndDelete(req.params.postId);
-	res.send(post);
+	let post 
+	try {
+		const post = await posts.findByIdAndDelete(req.params.postId);
+		res.send(post);
+	} 
+	catch {
+		if (!post) res.status(404).send('Could not find the specified user.')
+	}
 });
 
 module.exports = router;
