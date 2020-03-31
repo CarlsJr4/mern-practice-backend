59e6e076cf9b2c61b344ba9cf8957789c9cfbc1d
Carl Dungca <carl.dungca@yahoo.com>
2020-03-31 15:21:59 -0700

    Created RESTful API routes for posts (without validation)

--- routes/posts.js
+++ routes/posts.js
@@ -1,23 +1,33 @@
 // Worry about optimizing/cleaning up code later. Right now, just focus on building.
 const express = require('express');
 const router = express.Router();
+const posts = require('../models/posts');
 
-router.get('/', (req, res) => {
-	res.send('All posts.')
+router.get('/', async (req, res) => {
+	const allPosts = await posts.find();
+	res.send(allPosts);
 });
 
 // In this get request, we need to check if the ID exists
-router.get('/:postId', (req, res) => {
-	res.send(req.params.postId);
+router.get('/:postId', async (req, res) => {
+	const post = await posts.findById(req.params.postId);
+	res.send(post);
 });
 
 // In a post request, we need validation
-router.post('/', (req, res) => {
-	res.send('Post request sent.')
+router.post('/', async (req, res) => {
+	const newPost = new posts({
+		title: req.body.title,
+		author: req.body.author,
+		body: req.body.body
+	});
+	newPost.save();
+	res.send(newPost);
 });
 
-router.delete('/', (req, res) => {
-	res.send('Post deleted.')
+router.delete('/:postId', async (req, res) => {
+	const post = await posts.findByIdAndDelete(req.params.postId);
+	res.send(post);
 });
 
 module.exports = router;
