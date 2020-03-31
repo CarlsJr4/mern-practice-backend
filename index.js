db96b8f105cc4e417b94d7ae65167f0a0637335e
Carl Dungca <carl.dungca@yahoo.com>
2020-03-31 14:20:14 -0700

    Added boilerplate express setup code

---
+++ index.js
@@ -0,0 +1,13 @@
+// Your goal is to build a small social media application
+// There will be posts, users, comments, and likes
+// Build the backend here, have your React frontend interact with the web server
+// First, focus on backend only. Then, focus on frontend only. 
+// Your last step will be to make the two talk to eachother.
+// Then, you will deploy your app to the internet
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
