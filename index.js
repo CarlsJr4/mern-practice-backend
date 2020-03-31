3d720265d6422b6d6795bc53e2bf896f305d279f
Carl Dungca <carl.dungca@yahoo.com>
2020-03-31 14:59:02 -0700

    Setup mongodb driver

--- index.js
+++ index.js
@@ -5,13 +5,19 @@
 // Your last step will be to make the two talk to eachother.
 // Then, you will deploy your app to the internet
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+const db = mongoose.connection;
+
+mongoose.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('Connected to mongoDB'));
 
 const posts = require('./routes/posts');
 
 app.use('/api/posts', posts);
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Welcome to a placeholder social media site!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- routes/posts.js
+++ routes/posts.js
@@ -6,10 +6,12 @@ router.get('/', (req, res) => {
 	res.send('All posts.')
 });
 
+// In this get request, we need to check if the ID exists
 router.get('/:postId', (req, res) => {
 	res.send(req.params.postId);
 });
 
+// In a post request, we need validation
 router.post('/', (req, res) => {
 	res.send('Post request sent.')
 });
