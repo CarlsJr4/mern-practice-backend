12becb01eaaeb9400d24701ae3c689976fd0e3dc
Carl Dungca <carl.dungca@yahoo.com>
2020-04-01 14:18:38 -0700

    Installed cors and body-parser to handle form data

--- index.js
+++ index.js
@@ -4,21 +4,30 @@
 // First, focus on backend only. Then, focus on frontend only. 
 // Your last step will be to make the two talk to eachother.
 // Then, you will deploy your app to the internet
+
+// Imports
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const db = mongoose.connection;
 
+// Database
 mongoose.connect('mongodb://127.0.0.1:27017/socialmedia', { useNewUrlParser: true, useUnifiedTopology: true });
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('Connected to mongoDB'));
 
 const posts = require('./routes/posts');
 
+// Middleware
+app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/api/posts', posts);
 
+// Etc
 app.get('/', (req, res) => res.send('Welcome to a placeholder social media site!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,15 @@
-// Worry about optimizing/cleaning up code later. Right now, just focus on building.
 const express = require('express');
 const router = express.Router();
 const posts = require('../models/posts');
 const _ = require('lodash');
 
+// Default route
 router.get('/', async (req, res) => {
 	const allPosts = await posts.find();
 	res.send(allPosts);
 });
 
-// In this get request, we need to check if the ID exists
+// Get post by ID
 router.get('/:postId', async (req, res) => {
 	let post
 	try {
@@ -17,29 +17,30 @@ router.get('/:postId', async (req, res) => {
 		res.send(post);
 	}
 	catch {
-		if (!post) res.status(404).send('Could not find the specified user.')
+		if (!post) res.status(404).send('Could not find the specified user.');
 	}
 });
 
-// In a post request, we need validation
-router.post('/', async (req, res) => {
+// Create new post
+router.post('/', async (req, res, next) => {
 	try {
 		const newPost = new posts(_.pick(req.body, ['title', 'author', 'body']));
 		await newPost.save();
 		res.send(newPost);
 	} catch (ex) {
-		res.status(400).send(ex);
+		next(ex);
 	}
 });
 
+// Delete a post
 router.delete('/:postId', async (req, res) => {
-	let post 
+	let post;
 	try {
 		const post = await posts.findByIdAndDelete(req.params.postId);
 		res.send(post);
 	} 
 	catch {
-		if (!post) res.status(404).send('Could not find the specified user.')
+		if (!post) res.status(404).send('Could not find the specified user.');
 	}
 });
 
