12fdaa4306a7ec7df93f0c3ee5cf0c0b7a961d9a
Carl Dungca <carl.dungca@yahoo.com>
2020-04-03 13:23:15 -0700

    Added extra schema validation rules to posts

--- models/posts.js
+++ models/posts.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// Next step: Add validation to your social media posts
+// Also add ability to delete posts
 const postSchema = new Schema({
 	title: { 
 		type: String,
-		required: true
+		required: true,
+		maxlength: 100
 	},
 	author: { 
 		type: String,
-		required: true
+		required: true,
+		maxlength: 25
 	},
 	datePosted: {
 		type: Date,
