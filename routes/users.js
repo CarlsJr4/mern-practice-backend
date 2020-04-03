3da1b7c2866bb311490c0a013cbf3c939cb27916
Carl Dungca <carl.dungca@yahoo.com>
2020-04-03 15:16:06 -0700

    Implemented login route to send JWT after login

--- routes/users.js
+++ routes/users.js
@@ -6,13 +6,13 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 
-router.post('/', async (req, res) => {
+router.post('/register', async (req, res) => {
 	try {
 		const newUser = new users(_.pick(req.body, ['username', 'email', 'password']));
 		const hashed = await bcrypt.hash(newUser.password, 10);
 		newUser.password = hashed;
 		await newUser.save();
-		const token = jwt.sign({ id: newUser._id}, config.placeholderKey);
+		const token = await jwt.sign({ id: newUser._id}, config.placeholderKey);
 		// Access token sent to client. Client saves to localStorage. Then, server must check for the token? 
 		res.set('X-access-token', token).send(newUser);
 	}
@@ -20,5 +20,25 @@ router.post('/', async (req, res) => {
 		res.send(ex);
 	}
 });
+
+// Goal for login: determine if the user exists, compare hashed password, then send JWT
+router.post('/login', async (req, res) => {
+	try {
+		// Check if user exists
+		const user = await users.findOne({ email: req.body.email });
+		if (!user) res.status(404).send('User with specified email was not found.');
+
+		// Compared hashed password
+		const authenticated = await bcrypt.compare(req.body.password, user.password);
+		if (!authenticated) res.status(400).send('Username or password was incorrect');
+
+		// Send JWT
+		const token = await jwt.sign({ id: user._id}, config.placeholderKey);
+		res.set('X-access-token', token).send(user);
+	}
+	catch (ex) {
+		res.send(ex)
+	}
+})
 
 module.exports = router;
