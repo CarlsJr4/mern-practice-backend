2cf3c2a2a947a6c138387f21a63e87047399f301
Carl Dungca <carl.dungca@yahoo.com>
2020-04-03 13:56:42 -0700

    Utilized bcrypt to hash user passwords when creating new users

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const users = require('../models/users');
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 
 router.post('/', async (req, res) => {
 	try {
 		const newUser = new users(_.pick(req.body, ['username', 'email', 'password']));
+		const hashed = await bcrypt.hash(newUser.password, 10);
+		newUser.password = hashed;
 		await newUser.save()
 		res.send(newUser);
 	}
