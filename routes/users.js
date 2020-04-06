c6083c82c5ef7cfcd149cb1a7043647dc07eb7d6
Carl Dungca <carl.dungca@yahoo.com>
2020-04-05 17:32:29 -0700

    Renamed variables in register POST route so the client can access them

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,13 @@ const config = require('../config/config');
 
 router.post('/register', async (req, res) => {
 	try {
-		const newUser = new users(_.pick(req.body, ['username', 'email', 'password']));
-		const hashed = await bcrypt.hash(newUser.password, 10);
-		newUser.password = hashed;
-		await newUser.save();
-		const token = await jwt.sign({ id: newUser._id}, config.placeholderKey);
+		const user = new users(_.pick(req.body, ['username', 'email', 'password']));
+		const hashed = await bcrypt.hash(user.password, 10);
+		user.password = hashed;
+		await user.save();
+		const token = await jwt.sign({ id: user._id}, config.placeholderKey);
 		// Access token sent to client. Client saves to localStorage. Then, server must check for the token? 
-		res.set('X-access-token', token).send({newUser, token});
+		res.set('X-access-token', token).send({user, token});
 	}
 	catch (ex) {
 		res.send(ex);
